a2273b9ab6ce51a672eddba05b17263665f37a43
Tan Yi Jun <tanyijun6@gmail.com>
2024-08-05 16:49:03 +0800

    Rewrite api data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
 import { useEffect, useState } from 'react'
 import 'normalize.css'
 import './App.css'
+import Card from './components/Card';
 
 const dataDragonUrl = 'https://ddragon.leagueoflegends.com/cdn/14.15.1/data/en_US/champion.json';
 const INIT_NUM = 5;
+
+async function fetchChampionData() {
+  const url = dataDragonUrl;
+  const response = await fetch(url, {mode:'cors'});
+  const json = await response.json();
+  const data = json.data;
+
+  const array = [];
+
+  for (let key in data) {
+    const name = data[key].name;
+    const imageUrl = getChampionImageUrl(name);
+    array.push({name, imageUrl});
+  }
+
+  return array;
+}
+
+async function getRandomChampion(num) {
+  const championData = await fetchChampionData();
+  const randomChamps = [];
+  let count = 0;
+
+  while (count < num) {
+    const randomInt = Math.floor(Math.random() * championData.length);
+    if (!randomChamps.includes(championData[randomInt])) {
+      randomChamps.push(championData[randomInt]);
+      count++;
+    }
+  }
+
+  console.log(randomChamps);
+
+  return randomChamps;
+}
+
+function getChampionImageUrl(name) {
+  const urlPrefix = 'https://ddragon.leagueoflegends.com/cdn/img/champion/loading/'
+  const urlSuffix = '_0.jpg'
+  const imageUrl = urlPrefix + name + urlSuffix;
+
+  return imageUrl;
+}
 
 function GetChampionsData(url) {
     const [data, setData] = useState(null);
@@ -38,24 +82,26 @@ function GetChampionsData(url) {
 function App() {
   // const [champions, setChampions] = useState();
 
-  const champions = GetChampionsData(dataDragonUrl);
+  const randomChamps = getRandomChampion(INIT_NUM);
 
   return (
     <>
       <div>
-        <h1>Hello! World!</h1>
+        <h1>Title</h1>
         <div>
           <h2>Champions:</h2>
           <ul>
-            {
-              champions ? champions.map(champ => {
+            {/* {
+              randomChamps ? randomChamps.map(champ => {
                 return (
-                  <li key={champ.id}>
-                    {champ.name}
-                  </li>
+                  <Card 
+                    key={champ.id}
+                    name={champ.name}
+                    imageUrl={getChampionImageUrl(champ.name)}
+                  />
                 )
               }) : 'Loading...'
-            }
+            } */}
           </ul>
         </div>
       </div>

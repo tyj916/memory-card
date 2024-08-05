94567e5be7552addab6463246ff930e2c6c51c59
Tan Yi Jun <tanyijun6@gmail.com>
2024-08-05 17:16:56 +0800

    Seperate api data fetch from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,10 @@
 import { useEffect, useState } from 'react'
 import 'normalize.css'
 import './App.css'
+import getRandomChampion from './championData';
 import Card from './components/Card';
 
-const dataDragonUrl = 'https://ddragon.leagueoflegends.com/cdn/14.15.1/data/en_US/champion.json';
 const INIT_NUM = 5;
-
-async function fetchChampionData() {
-  const url = dataDragonUrl;
-  const response = await fetch(url, {mode:'cors'});
-  const json = await response.json();
-  const data = json.data;
-
-  const array = [];
-
-  for (let key in data) {
-    const id = data[key].id;
-    const name = data[key].name;
-    const imageUrl = getChampionImageUrl(name);
-    array.push({id, name, imageUrl});
-  }
-
-  return array;
-}
-
-async function getRandomChampion(num) {
-  const championData = await fetchChampionData();
-  const randomChamps = [];
-  let count = 0;
-
-  while (count < num) {
-    const randomInt = Math.floor(Math.random() * championData.length);
-    if (!randomChamps.includes(championData[randomInt])) {
-      randomChamps.push(championData[randomInt]);
-      count++;
-    }
-  }
-
-  return randomChamps;
-}
-
-function getChampionImageUrl(name) {
-  const urlPrefix = 'https://ddragon.leagueoflegends.com/cdn/img/champion/loading/'
-  const urlSuffix = '_0.jpg'
-  const imageUrl = urlPrefix + name + urlSuffix;
-
-  return imageUrl;
-}
 
 function App() {
   const [champions, setChampions] = useState([]);
---
+++ src/championData.js
@@ -0,0 +1,45 @@
+const dataDragonUrl = 'https://ddragon.leagueoflegends.com/cdn/14.15.1/data/en_US/champion.json';
+
+function getChampionImageUrl(name) {
+  const urlPrefix = 'https://ddragon.leagueoflegends.com/cdn/img/champion/loading/'
+  const urlSuffix = '_0.jpg'
+  const imageUrl = urlPrefix + name + urlSuffix;
+
+  return imageUrl;
+}
+
+async function fetchChampionData() {
+  const url = dataDragonUrl;
+  const response = await fetch(url, {mode:'cors'});
+  const json = await response.json();
+  const data = json.data;
+
+  const array = [];
+
+  for (let key in data) {
+    const id = data[key].id;
+    const name = data[key].name;
+    const imageUrl = getChampionImageUrl(name);
+    array.push({id, name, imageUrl});
+  }
+
+  return array;
+}
+
+async function getRandomChampion(num) {
+  const championData = await fetchChampionData();
+  const randomChamps = [];
+  let count = 0;
+
+  while (count < num) {
+    const randomInt = Math.floor(Math.random() * championData.length);
+    if (!randomChamps.includes(championData[randomInt])) {
+      randomChamps.push(championData[randomInt]);
+      count++;
+    }
+  }
+
+  return randomChamps;
+}
+
+export default getRandomChampion;
